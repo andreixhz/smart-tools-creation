b608c1ee1c1494b52b6cf12fc8af4ac1fab5637e
Andrei goncalves <andrei.goncalves@modalgr.com.br>
2020-12-08 22:58:41 -0300

    Config file

---
+++ src/Data.js
@@ -0,0 +1,27 @@
+module.exports = {
+    "main":"/src/",
+    "files":[
+        "/index.js",
+        "/style.css"
+    ],
+    "type":[
+        "page/",
+        "component/"
+    ],
+    "data":`import React from 'react';
+import './styles.css';
+
+function TEMP_NAME () {
+    return (
+        <div className={TEMP_NAME}>
+            <h1>TEMP_NAME</h1>
+        </dib>
+    );
+}
+
+export default TEMP_NAME;
+`,
+    "style":`.TEMP_NAME{
+    margin-top:10px;
+}`
+}
