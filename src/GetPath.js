bbc53ac1a004527d2aa849eae90550c6bef1959f
Andrei Gonçalves <andrei.goncalves@modalgr.com.br>
2020-12-13 14:36:23 -0300

    Bug fix

--- src/GetPath.js
+++ src/GetPath.js
@@ -3,25 +3,26 @@ const vscode = require('vscode');
 async function GetPath() {
     const workspaceExists = vscode.workspace.workspaceFolders && vscode.workspace.workspaceFolders.length > 0;
     if (!vscode.window.activeTextEditor && !workspaceExists) {
-        vscode.window.showInformationMessage('You do not have any workspaces open.');
+        vscode.window.showInformationMessage('Open any workspace');
         return undefined;
     }
 
-    if (vscode.window.activeTextEditor) {
-		
-		return vscode.window.activeTextEditor.document.uri.path.substring(1);
-		
-    } else if (vscode.workspace.workspaceFolders.length === 1) {
-        return vscode.workspace.workspaceFolders[0].uri.path.substring(1);
+    if (vscode.workspace.workspaceFolders.length === 1) {
+        return {
+            path: vscode.workspace.workspaceFolders[0].uri,
+            type: 'workspace'
+        };
     } else {
-
         const ws = await vscode.window.showWorkspaceFolderPick();
 
         if(!ws) {
             return undefined;
         }
 
-        return ws.uri.path.substring(1);
+        return {
+            path: ws.uri,
+            type: 'workspace'
+        };
     }
 }
 
